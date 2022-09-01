cb5e0a00042e3023d2a57aa48ab678b175aafac9
hello744 <85447595+hello744@users.noreply.github.com>
2022-09-01 09:39:57 -0500

    Added Dropdown

--- script.js
+++ script.js
@@ -58,4 +58,19 @@ clearIcon.addEventListener("click", () => {
 })
 
 
+function dropInfo(){
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
