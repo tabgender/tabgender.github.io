2fb0c54dbfa73ba7fa0bd711cf9f9f7abc007ead
ShivumB <73792393+ShivumB@users.noreply.github.com>
2022-08-31 21:34:25 -0500

    Reorganized css

--- script.js
+++ script.js
@@ -33,55 +33,10 @@ class DigitalClock {
   }
 }
 
-class ColorChange {
-
-  constructor(element) {
-    this.element = element;
-    this.currentColor = [0, 0, 0];
-    this.targetColor = [255, 255, 255];
-    this.blend = 0;
-  }
-
-  start() {
-    this.update();
-
-    setInterval(() => {this.update();}, 0);
-    
-  }
-
-update() {
-
-  if(this.blend >= 0.6) {
-    this.targetColor[0] = Math.random()*255;
-    this.targetColor[1] = Math.random()*255;
-    this.targetColor[2] = Math.random()*255;
-
-    this.blend = 0;
-  }
-
-  this.currentColor[0] = (1-this.blend)*this.currentColor[0] + this.blend*this.targetColor[0];
-  this.currentColor[1] = (1-this.blend)*this.currentColor[1] + this.blend*this.targetColor[1];
-  this.currentColor[2] = (1-this.blend)*this.currentColor[2] + this.blend*this.targetColor[2];
-
-  this.blend += 0.001;
-
-  this.element.style.setProperty('--a-color', "rgb(" + Math.floor(this.currentColor[0]) +"," + Math.floor(this.currentColor[1]) + "," + Math.floor(this.currentColor[2]) + ")")    
-  }  
-}
-
-
-
-
 const clockElement = document.querySelector(".clock");
 const clockObject = new DigitalClock(clockElement);
 
-const colorChange = new ColorChange(clockElement);
-colorChange.start();
-
 clockObject.start();
-
-
-
 
 const clearIcon = document.querySelector(".clear-icon");
 const searchBar = document.querySelector(".search");
