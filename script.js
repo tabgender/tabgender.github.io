9d6778039827dd1f6773ba5d3b39b1d00ff06c1b
ShivumB <112597214+tabgender@users.noreply.github.com>
2022-09-01 10:05:53 -0500

    Fixed oddities (for real, this time) (hopefully)

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 class DigitalClock {
-
+ 
   constructor(element) {
     this.element = element;
   }
-
+ 
   start() {
     this.update();
-
+ 
     setInterval(() => {
       this.update();
     }, 500);
   }
-
+ 
   update() {
     const parts = this.getTimeParts();
     const minuteFormatted = parts.minute.toString().padStart(2, "0");
     const timeFormatted = `${parts.hour}:${minuteFormatted}`;
     const amPm = parts.isAm ? "AM" : "PM";
-
+ 
     this.element.querySelector(".clock-time").textContent = timeFormatted;
     this.element.querySelector(".clock-ampm").textContent = amPm;
   }
-
+ 
   getTimeParts() {
     const now = new Date();
-
+ 
     return {
       hour: now.getHours() % 12 || 12,
       minute: now.getMinutes(),
@@ -32,18 +32,18 @@ class DigitalClock {
     };
   }
 }
-
+ 
 const clockElement = document.querySelector(".clock");
 const clockObject = new DigitalClock(clockElement);
-
-
+ 
+ 
 clockObject.start();
-
-
-
+ 
+ 
+ 
 const clearIcon = document.querySelector(".clear-icon");
 const searchBar = document.querySelector(".search");
-
+ 
 searchBar.addEventListener("keyup", () => {
   if (searchBar.value && clearIcon.style.visibility != "visible") {
     clearIcon.style.visibility = "visible";
@@ -51,26 +51,8 @@ searchBar.addEventListener("keyup", () => {
     clearIcon.style.visibility = "hidden";
   }
 });
-
+ 
 clearIcon.addEventListener("click", () => {
   searchBar.value = "";
   clearIcon.style.visibility = "hidden";
 })
-
-
-function dropInfo(){
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
