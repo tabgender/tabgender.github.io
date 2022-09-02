c4f5fc3b757c171c7f7b576cbb7702fddf3c9562
ShivumB <112597214+tabgender@users.noreply.github.com>
2022-09-02 16:22:54 -0500

    Implemented dynamic time, removed unnecessary HTML

--- script.js
+++ script.js
@@ -1,79 +1,72 @@
-class DigitalClock {
- 
-    constructor(element) {
-      this.element = element;
-    }
-   
-    start() {
+class Clock {
+
+  constructor(time, date) {
+    this.time = time;
+    this.date = date;
+  }
+
+  start() {
+    this.update();
+    const d = new Date();
+
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var monthsOfYear = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+
+    this.date.textContent = daysOfWeek[d.getDay()] + ", " + monthsOfYear[d.getMonth()] + " " + d.getDate();
+
+    setInterval(() => {
       this.update();
-   
-      setInterval(() => {
-        this.update();
-      }, 500);
-    }
-   
-    update() {
-      const parts = this.getTimeParts();
-      const minuteFormatted = parts.minute.toString().padStart(2, "0");
-      const timeFormatted = `${parts.hour}:${minuteFormatted}`;
-      const amPm = parts.isAm ? "AM" : "PM";
-   
-      this.element.querySelector(".clock-time").textContent = timeFormatted;
-      this.element.querySelector(".clock-ampm").textContent = amPm;
-    }
-   
-    getTimeParts() {
-      const now = new Date();
-   
-      return {
-        hour: now.getHours() % 12 || 12,
-        minute: now.getMinutes(),
-        isAm: now.getHours() < 12
-      };
+    }, 500);
+  }
+
+  update() {
+    const d = new Date();
+
+    if (d.getHours() > 12) {
+      this.time.textContent = (d.getHours()-12) + ":" + d.getMinutes() + " pm";
+    } else {
+      this.time.textContent = d.getHours() + ":" + d.getMinutes() + " am";
     }
   }
-   
-  const clockElement = document.querySelector(".clock");
-  const clockObject = new DigitalClock(clockElement);
-   
-   
-  clockObject.start();
-   
-   
-   
-  const clearIcon = document.querySelector(".clear-icon");
-  const searchBar = document.querySelector(".search");
-   
-  searchBar.addEventListener("keyup", () => {
-    if (searchBar.value && clearIcon.style.visibility != "visible") {
-      clearIcon.style.visibility = "visible";
-    } else if (!searchBar.value) {
-      clearIcon.style.visibility = "hidden";
-    }
-  });
-   
-  clearIcon.addEventListener("click", () => {
-    searchBar.value = "";
+
+}
+console.log(document.getElementById("time"));
+const clock = new Clock(document.getElementById("time"), document.getElementById("date"));
+clock.start();
+
+const clearIcon = document.querySelector(".clear-icon");
+const searchBar = document.querySelector(".search");
+
+searchBar.addEventListener("keyup", () => {
+  if (searchBar.value && clearIcon.style.visibility != "visible") {
+    clearIcon.style.visibility = "visible";
+  } else if (!searchBar.value) {
     clearIcon.style.visibility = "hidden";
-  })
-  
-  function dropInfo(){
-    document.getElementById("myDropdown").classList.toggle("show");
   }
-  
-  window.oneclick = function(event){
-   if(!event.target.matches('.dropbtn')){
+});
+
+clearIcon.addEventListener("click", () => {
+  searchBar.value = "";
+  clearIcon.style.visibility = "hidden";
+})
+
+function dropInfo() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.oneclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
-    for(var i = 0; i < dropdowns.length; i++){
+    for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if(openDropdown.classList.container('show')){
+      if (openDropdown.classList.container('show')) {
         openDropdown.classList.remove('show');
       }
     }
-   }
-  
-  
-  
-  
   }
+
+
+
+
+}
