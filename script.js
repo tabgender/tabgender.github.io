c4de67db4a992868016f4fe21927039473c699e1
ShivumB <112597214+tabgender@users.noreply.github.com>
2022-09-02 15:40:25 -0500

    Complete UI Overhaul - replaced with Live Simply, Purple theme

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
 class DigitalClock {
  
-  constructor(element) {
-    this.element = element;
-  }
- 
-  start() {
-    this.update();
- 
-    setInterval(() => {
+    constructor(element) {
+      this.element = element;
+    }
+   
+    start() {
       this.update();
-    }, 500);
-  }
- 
-  update() {
-    const parts = this.getTimeParts();
-    const minuteFormatted = parts.minute.toString().padStart(2, "0");
-    const timeFormatted = `${parts.hour}:${minuteFormatted}`;
-    const amPm = parts.isAm ? "AM" : "PM";
- 
-    this.element.querySelector(".clock-time").textContent = timeFormatted;
-    this.element.querySelector(".clock-ampm").textContent = amPm;
-  }
- 
-  getTimeParts() {
-    const now = new Date();
- 
-    return {
-      hour: now.getHours() % 12 || 12,
-      minute: now.getMinutes(),
-      isAm: now.getHours() < 12
-    };
-  }
-}
- 
-const clockElement = document.querySelector(".clock");
-const clockObject = new DigitalClock(clockElement);
- 
- 
-clockObject.start();
- 
- 
- 
-const clearIcon = document.querySelector(".clear-icon");
-const searchBar = document.querySelector(".search");
- 
-searchBar.addEventListener("keyup", () => {
-  if (searchBar.value && clearIcon.style.visibility != "visible") {
-    clearIcon.style.visibility = "visible";
-  } else if (!searchBar.value) {
-    clearIcon.style.visibility = "hidden";
-  }
-});
- 
-clearIcon.addEventListener("click", () => {
-  searchBar.value = "";
-  clearIcon.style.visibility = "hidden";
-})
-
-function dropInfo(){
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-window.oneclick = function(event){
- if(!event.target.matches('.dropbtn')){
-  var dropdowns = document.getElementsByClassName("dropdown-content");
-  var i;
-  for(var i = 0; i < dropdowns.length; i++){
-    var openDropdown = dropdowns[i];
-    if(openDropdown.classList.container('show')){
-      openDropdown.classList.remove('show');
+   
+      setInterval(() => {
+        this.update();
+      }, 500);
+    }
+   
+    update() {
+      const parts = this.getTimeParts();
+      const minuteFormatted = parts.minute.toString().padStart(2, "0");
+      const timeFormatted = `${parts.hour}:${minuteFormatted}`;
+      const amPm = parts.isAm ? "AM" : "PM";
+   
+      this.element.querySelector(".clock-time").textContent = timeFormatted;
+      this.element.querySelector(".clock-ampm").textContent = amPm;
+    }
+   
+    getTimeParts() {
+      const now = new Date();
+   
+      return {
+        hour: now.getHours() % 12 || 12,
+        minute: now.getMinutes(),
+        isAm: now.getHours() < 12
+      };
     }
   }
- }
-
-
-
-
-}
+   
+  const clockElement = document.querySelector(".clock");
+  const clockObject = new DigitalClock(clockElement);
+   
+   
+  clockObject.start();
+   
+   
+   
+  const clearIcon = document.querySelector(".clear-icon");
+  const searchBar = document.querySelector(".search");
+   
+  searchBar.addEventListener("keyup", () => {
+    if (searchBar.value && clearIcon.style.visibility != "visible") {
+      clearIcon.style.visibility = "visible";
+    } else if (!searchBar.value) {
+      clearIcon.style.visibility = "hidden";
+    }
+  });
+   
+  clearIcon.addEventListener("click", () => {
+    searchBar.value = "";
+    clearIcon.style.visibility = "hidden";
+  })
+  
+  function dropInfo(){
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.oneclick = function(event){
+   if(!event.target.matches('.dropbtn')){
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for(var i = 0; i < dropdowns.length; i++){
+      var openDropdown = dropdowns[i];
+      if(openDropdown.classList.container('show')){
+        openDropdown.classList.remove('show');
+      }
+    }
+   }
+  
+  
+  
+  
+  }
