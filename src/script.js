e22fd22a9932d62075b1d49f91bff22ba43ee036
ShivumB <112597214+tabgender@users.noreply.github.com>
2022-11-26 10:59:43 -0600

    Redoing pipeline

---
+++ src/script.js
@@ -0,0 +1,24 @@
+//time
+var time = document.getElementById("time");
+var date = document.getElementById("date");
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthsOfYear = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sep.", "Oct.", "Nov.", "Dec."];
+
+function update(time, date) {
+  const d = new Date();
+
+  if (d.getHours() > 12) {
+    time.textContent = (d.getHours() - 12) + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + " pm";
+  } else if (d.getHours() == 12) {
+    time.textContent = 12 + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + " pm";
+  } else if(d.getHours() == 0){
+    time.textContent = 12 + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + " am";
+  } else {
+    time.textContent = d.getHours() + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + " am";
+  }
+
+  date.textContent = daysOfWeek[d.getDay()] + ", " + monthsOfYear[d.getMonth()] + " " + d.getDate();
+}
+
+update(time, date);
+setInterval(update, 750, time, date);
